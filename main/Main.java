a1b7c8f99852762c799ac2d147899d1b635316fc
Olga Fomochkina <olga2992930@gmail.com>
2024-09-12 11:15:39 +0200

    3

--- main/Main.java
+++ main/Main.java
@@ -1,27 +1,16 @@
 package main;
 import command.CommandManager;
-import java.util.Scanner;
+
 public class Main {
-    public boolean running =true;
+    public boolean running = true;
     public CommandManager commandManager = new CommandManager(this);
 
+    
     public static void main(String[] args) {
-        Scanner myScanner=new Scanner(System.in);
-        String playerName;
-       // int choice;
-
         System.out.println("Välkommen till Spelet!");
-        System.out.println("Skriv ditt namn...");
-        playerName=myScanner.nextLine();
-        System.out.println("Hej "+playerName+"!");
-        System.out.println("Du vaknar upp i ett rum. Utforska rummet.");
-        // System.out.println("Här ser du en kista. Hur agerar du?");
-        // System.out.println("Skriv en av följande kommandon: ");
-        // System.out.println("Skriv 1 för `help`");
-        // System.out.println("Skriv 2 för `search`");
-        // System.out.println("Skriv 3 för `open`");
-        // System.out.println("Skriv 4 för `pickup`");
-        // System.out.println("Skriv 5 för `say`");
+        System.out.println("Du vaknar upp i ett öde hus. För att ta dig ut behöver du hitta 3 nycklar som ligger gömda.");
+        System.out.println("I huset finns det olika objekt att söka igenom för att hitta nycklarna.");
+        System.out.println("Skriv 'help' för att visa de olika kommandona.");
 
         Main main = new Main();
 
@@ -31,44 +20,37 @@ public class Main {
             } catch (Exception exception) {
                 System.out.println(exception.getMessage());
             }
-
         }
-
-
-    //     choice = myScanner.nextInt();
-
-    //   //   1.help
-    // if (choice == 1) {
-    //    System.out.println("1.help");
-    //    System.out.println("Här ska visas information om alla kommandon");
-    // }
-
-    // //   2.search
-    // else if (choice == 2) {
-    //     System.out.println("I rummet hittar du en kista.");
-    //     System.out.println("Hur tänker du agera? (1,3,4 eller 5`?)");
-    // }  
-    
-    // //   3.open
-    // else if (choice == 3) {
         
-    // }        
-
-    // //   4.pickup
-    // else if (choice == 4) {
-        
-    // }  
-    
-    //  //   5.say
-    //  else if (choice == 5) {
-        
-    //  }     
-
-    //  else   {
-    // System.out.println("Fel! Tryck 1,2,3,4 eller 5");  
-        
-    //  }     
-
     }
+    
 }
+/*Steg 1. Printa ut text
+Du vaknar upp i ett öde hus. För att ta dig ut behöver du hitta 3 nycklar som ligger gömda. 
+I huset finns det olika objekt att söka igenom för att hitta nycklarna. 
+Skriv 'help' för att visa de olika kommandona.
+ 
+ 
+Steg 2. Help
+Funktion som printar ut text om vilka kommandon som finns.
+ 
+Steg 3. - Search
+Funktion som printar ut 5 olika platser (skåp, påse, kista, ugn, vindslucka t.ex.)
 
+
+
+Killarnas del
+
+Steg 4. Open
+ 
+Här ska man kunna skriva “Open”. När man skrivit open har vi en funktion som ser om det finns en nyckel där (true/false
+ 
+(Sen kan man skriva search igen)
+ 
+Steg 5.  Pick up
+
+Här tar man nyckeln. skal sparas.
+
+Steg 6. Gåta 
+
+När det finns en nyckel kvar så ska man få upp en gåta. Hur görs denna? Ja och nej svar?*/
